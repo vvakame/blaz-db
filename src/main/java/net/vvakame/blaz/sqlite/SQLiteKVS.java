497166e26734ad270fbc69faec27de149461f097
vvakame <vvakame+dev@gmail.com>
2011-05-06 18:48:59 +0900

    SQLiteKVSにput実装を追加

--- src/main/java/net/vvakame/blaz/sqlite/SQLiteKVS.java
+++ src/main/java/net/vvakame/blaz/sqlite/SQLiteKVS.java
@@ -1,6 +1,7 @@
 package net.vvakame.blaz.sqlite;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -13,12 +14,17 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import static net.vvakame.blaz.sqlite.KvsOpenHelper.*;
 
 /**
  * SQLiteによるKVSの実装
  * @author vvakame
  */
 public class SQLiteKVS implements IKeyValueStore {
+
+	static final String DB_NAME = "blaz.db";
+
+	static final int DB_VERSION = 1;
 
 	Context mContext;
 
@@ -32,25 +38,25 @@ public class SQLiteKVS implements IKeyValueStore {
 	 */
 	public SQLiteKVS(Context context) {
 		mContext = context.getApplicationContext();
-		mDb = new KvsOpenHelper(mContext, "blaz.db", 1).getWritableDatabase();
+		mDb = new KvsOpenHelper(mContext, DB_NAME, DB_VERSION).getWritableDatabase();
 	}
 
 	@Override
 	public void put(Entity entity) {
 		{
 			ContentValues values = convKeyToValues(entity.getKey());
-			mDb.insert("KEYS", null, values);
+			mDb.insert(TABLE_KEYS, null, values);
 		}
 		List<ContentValues> list = convPropertiesToValues(entity.getKey(), entity.getProperties());
 		for (ContentValues values : list) {
-			mDb.insert("VALUES", null, values);
+			mDb.insert(TABLE_VALUES, null, values);
 		}
 	}
 
 	@Override
 	public Entity get(Key key) {
 		Cursor c;
-		c = mDb.query("KEYS", null, "KEY_STR = ?", new String[] {
+		c = mDb.query(TABLE_KEYS, null, "KEY_STR = ?", new String[] {
 			KeyUtil.keyToString(key)
 		}, null, null, null);
 		if (!c.moveToFirst()) {
@@ -58,20 +64,21 @@ public class SQLiteKVS implements IKeyValueStore {
 			throw new IllegalArgumentException();
 		}
 		Key newKey = new Key();
-		newKey.setId(c.getLong(c.getColumnIndex("ID")));
-		newKey.setName(c.getString(c.getColumnIndex("NAME")));
+		newKey.setId(c.getLong(c.getColumnIndex(COL_ID)));
+		newKey.setName(c.getString(c.getColumnIndex(COL_NAME)));
 
-		c = mDb.query("VALUES", null, "KEY_STR = ?", new String[] {
+		c = mDb.query(TABLE_VALUES, null, "KEY_STR = ?", new String[] {
 			KeyUtil.keyToString(key)
 		}, null, null, null);
+
 		Entity entity = new Entity();
 		entity.setKey(newKey);
-		int nameIdx = c.getColumnIndex("NAME");
-		int typeIdx = c.getColumnIndex("TYPE");
-		int valStrIdx = c.getColumnIndex("VAL_STR");
-		int valIntIdx = c.getColumnIndex("VAL_INT");
-		int valRealIdx = c.getColumnIndex("VAL_REAL");
-		int valBlobIdx = c.getColumnIndex("VAL_BLOB");
+		final int nameIdx = c.getColumnIndex(COL_NAME);
+		final int typeIdx = c.getColumnIndex(COL_TYPE);
+		final int valStrIdx = c.getColumnIndex(COL_VALUE_STRING);
+		final int valIntIdx = c.getColumnIndex(COL_VALUE_INTEGER);
+		final int valRealIdx = c.getColumnIndex(COL_VALUE_REAL);
+		final int valBlobIdx = c.getColumnIndex(COL_VALUE_BLOB);
 		if (!c.moveToFirst()) {
 			return entity;
 		}
@@ -79,7 +86,7 @@ public class SQLiteKVS implements IKeyValueStore {
 			String name = c.getString(nameIdx);
 			String type = c.getString(typeIdx);
 			Object value;
-			if ("String".equals(type)) {
+			if (T_STRING.equals(type)) {
 				value = c.getString(valStrIdx);
 			} else {
 				throw new UnsupportedOperationException();
@@ -98,37 +105,131 @@ public class SQLiteKVS implements IKeyValueStore {
 		ContentValues values = new ContentValues();
 
 		if (key.getName() == null) {
-			values.put("ID", key.getId());
+			values.put(COL_ID, key.getId());
 		} else {
-			values.put("NAME", key.getName());
+			values.put(COL_NAME, key.getName());
 		}
-		values.put("KIND", key.getKind());
-		values.put("KEY_STR", KeyUtil.keyToString(key));
-		values.put("PARENT_KEY", KeyUtil.keyToString(key.getParent()));
+		values.put(COL_KIND, key.getKind());
+		values.put(COL_KEY_STRING, KeyUtil.keyToString(key));
+		values.put(COL_PARENT_KEY, KeyUtil.keyToString(key.getParent()));
 
 		return values;
 	}
 
 	static List<ContentValues> convPropertiesToValues(Key key, Map<String, Object> properties) {
-		// TODO ValuesがListの場合
-		// FIXME 今はStringがValueの場合のみ考える
 		List<ContentValues> values = new ArrayList<ContentValues>(properties.keySet().size());
-		for (String keyStr : properties.keySet()) {
-			Object obj = properties.get(keyStr);
-			if (obj instanceof String) {
-				ContentValues value = new ContentValues();
-				value.put("KEY_STR", KeyUtil.keyToString(key));
-				value.put("KIND", key.getKind());
-				value.put("NAME", keyStr);
-				value.put("TYPE", "String");
-				// TODO 文字コードを指定したほうがいい
-				value.put("VAL_STR", (String) obj);
-				values.add(value);
+		for (String name : properties.keySet()) {
+			Object obj = properties.get(name);
+			if (obj instanceof Collection) {
+				convPropertiesToValuesSub(key, values, name, (Collection<?>) obj);
 			} else {
-				throw new UnsupportedOperationException();
+				convPropertiesToValuesSub(key, values, name, obj);
 			}
 		}
 
 		return values;
+	}
+
+	static void convPropertiesToValuesSub(Key key, List<ContentValues> values, String name,
+			Object obj) {
+		ContentValues value = new ContentValues();
+		value.put(COL_KEY_STRING, KeyUtil.keyToString(key));
+		value.put(COL_KIND, key.getKind());
+		value.put(COL_NAME, name);
+
+		if (obj == null) {
+			value.put(COL_TYPE, T_NULL);
+		} else if (obj instanceof String) {
+			value.put(COL_TYPE, T_STRING);
+			value.put(COL_VALUE_STRING, (String) obj);
+		} else if (obj instanceof Byte) {
+			value.put(COL_TYPE, T_LONG);
+			value.put(COL_VALUE_INTEGER, (Byte) obj);
+		} else if (obj instanceof Short) {
+			value.put(COL_TYPE, T_LONG);
+			value.put(COL_VALUE_INTEGER, (Short) obj);
+		} else if (obj instanceof Integer) {
+			value.put(COL_TYPE, T_LONG);
+			value.put(COL_VALUE_INTEGER, (Integer) obj);
+		} else if (obj instanceof Long) {
+			value.put(COL_TYPE, T_LONG);
+			value.put(COL_VALUE_INTEGER, (Long) obj);
+		} else if (obj instanceof Boolean) {
+			value.put(COL_TYPE, T_BOOLEAN);
+			if ((Boolean) obj) {
+				value.put(COL_VALUE_STRING, T_V_BOOLEAN_TRUE);
+			} else {
+				value.put(COL_VALUE_STRING, T_V_BOOLEAN_FALSE);
+			}
+		} else if (obj instanceof Float) {
+			value.put(COL_TYPE, T_DOUBLE);
+			value.put(COL_VALUE_REAL, (Float) obj);
+		} else if (obj instanceof Double) {
+			value.put(COL_TYPE, T_DOUBLE);
+			value.put(COL_VALUE_REAL, (Double) obj);
+		} else if (obj instanceof Key) {
+			value.put(COL_TYPE, T_KEY);
+			value.put(COL_VALUE_STRING, KeyUtil.keyToString((Key) obj));
+		} else if (obj instanceof byte[]) {
+			value.put(COL_TYPE, T_BYTES);
+			value.put(COL_VALUE_BLOB, (byte[]) obj);
+		} else {
+			throw new UnsupportedOperationException();
+		}
+		values.add(value);
+	}
+
+	static void convPropertiesToValuesSub(Key key, List<ContentValues> values, String name,
+			Collection<?> collection) {
+		Object[] list = collection.toArray();
+		for (int i = 0; i < list.length; i++) {
+			Object obj = list[i];
+			ContentValues value = new ContentValues();
+			value.put(COL_KEY_STRING, KeyUtil.keyToString(key));
+			value.put(COL_KIND, key.getKind());
+			value.put(COL_NAME, name);
+			value.put(COL_SEQ, i);
+
+			if (obj == null) {
+				value.put(COL_TYPE, T_L_NULL);
+			} else if (obj instanceof String) {
+				value.put(COL_TYPE, T_L_STRING);
+				value.put(COL_VALUE_STRING, (String) obj);
+			} else if (obj instanceof Byte) {
+				value.put(COL_TYPE, T_L_LONG);
+				value.put(COL_VALUE_INTEGER, (Byte) obj);
+			} else if (obj instanceof Short) {
+				value.put(COL_TYPE, T_L_LONG);
+				value.put(COL_VALUE_INTEGER, (Short) obj);
+			} else if (obj instanceof Integer) {
+				value.put(COL_TYPE, T_L_LONG);
+				value.put(COL_VALUE_INTEGER, (Integer) obj);
+			} else if (obj instanceof Long) {
+				value.put(COL_TYPE, T_L_LONG);
+				value.put(COL_VALUE_INTEGER, (Long) obj);
+			} else if (obj instanceof Boolean) {
+				value.put(COL_TYPE, T_L_BOOLEAN);
+				if ((Boolean) obj) {
+					value.put(COL_VALUE_STRING, T_V_BOOLEAN_TRUE);
+				} else {
+					value.put(COL_VALUE_STRING, T_V_BOOLEAN_FALSE);
+				}
+			} else if (obj instanceof Float) {
+				value.put(COL_TYPE, T_L_DOUBLE);
+				value.put(COL_VALUE_REAL, (Float) obj);
+			} else if (obj instanceof Double) {
+				value.put(COL_TYPE, T_L_DOUBLE);
+				value.put(COL_VALUE_REAL, (Double) obj);
+			} else if (obj instanceof Key) {
+				value.put(COL_TYPE, T_L_KEY);
+				value.put(COL_VALUE_STRING, KeyUtil.keyToString((Key) obj));
+			} else if (obj instanceof byte[]) {
+				value.put(COL_TYPE, T_L_BYTES);
+				value.put(COL_VALUE_BLOB, (byte[]) obj);
+			} else {
+				throw new UnsupportedOperationException();
+			}
+			values.add(value);
+		}
 	}
 }
--- src/test/java/net/vvakame/blaz/DatastoreTest.java
+++ src/test/java/net/vvakame/blaz/DatastoreTest.java
@@ -1,5 +1,8 @@
 package net.vvakame.blaz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.vvakame.blaz.sqlite.SQLiteKVS;
 
 import org.junit.Before;
@@ -20,6 +23,51 @@ import static org.junit.Assert.*;
  */
 @RunWith(RobolectricTestRunner.class)
 public class DatastoreTest {
+
+	/**
+	 * {@link Datastore#put(Entity)} に対して対応しているはずの全ての型を突っ込む.
+	 * @author vvakame
+	 */
+	@Test
+	public void put() {
+		Entity entity = new Entity();
+		entity.setKey(KeyUtil.createKey("hoge", "piyo"));
+		entity.setProperty("null", null);
+		entity.setProperty("String", "str");
+		entity.setProperty("Byte", (byte) 1);
+		entity.setProperty("Short", (short) 2);
+		entity.setProperty("Integer", 3);
+		entity.setProperty("Long", 4L);
+		entity.setProperty("BooleanT", true);
+		entity.setProperty("BooleanF", false);
+		entity.setProperty("Float", 1.1f);
+		entity.setProperty("Double", 2.2);
+		entity.setProperty("Key", KeyUtil.createKey("hoge", "puyo"));
+		entity.setProperty("byte[]", new byte[] {
+			1,
+			2,
+			3
+		});
+		List<Object> list = new ArrayList<Object>();
+		list.add(null);
+		list.add("str");
+		list.add((byte) 1);
+		list.add((short) 2);
+		list.add(3);
+		list.add(4L);
+		list.add(true);
+		list.add(false);
+		list.add(1.1f);
+		list.add(2.2);
+		list.add(KeyUtil.createKey("hoge", "payo"));
+		list.add(new byte[] {
+			1,
+			2,
+			3
+		});
+		entity.setProperty("List", list);
+		Datastore.put(entity);
+	}
 
 	/**
 	 * {@link Datastore#put(Entity)} と {@link Datastore#get(Key)} の動作確認
