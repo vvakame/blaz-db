96b97d1f38adb7137836f21f30b6a44f4f8ca44b
vvakame <vvakame+dev@gmail.com>
2011-07-30 18:40:32 +0900

    mvn.shの変更 blazdb-sqlite をビルド対象に含めるようにした

--- mvn.sh
+++ mvn.sh
@@ -1,4 +1,7 @@
 mvn clean eclipse:clean eclipse:eclipse test install
+cd blazdb-sqlite
+mvn clean eclipse:clean eclipse:eclipse test install
+cd ../
 cd blazdb-generated
 mvn clean eclipse:clean eclipse:eclipse
 cd ../
