1c9e58747e666b96e88781c101815d18474a5426
vvakame <vvakame+dev@gmail.com>
2012-12-12 18:55:03 +0900

    Added module blazdb-android-sqlite to root pom.xml

--- mvn.sh
+++ mvn.sh
@@ -1,11 +1,7 @@
 mvn clean eclipse:clean eclipse:eclipse test install -DdownloadSources=true && \
-cd blazdb-android-sqlite && \
-mvn clean eclipse:clean eclipse:eclipse test install -DdownloadSources=true && \
-cd ../ && \
 cd blazdb-generated && \
 mvn clean eclipse:clean eclipse:eclipse -DdownloadSources=true && \
 cd ../ && \
 cd tryout && \
 mvn clean eclipse:clean eclipse:eclipse -DdownloadSources=true && \
 cd ..
-
