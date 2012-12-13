6ffaa7aee9bc0d8331a119c5304e314c41305372
vvakame <vvakame+dev@gmail.com>
2012-12-13 16:56:13 +0900

    Renamed blazdb-generated to blazdb-test-generated

--- mvn.sh
+++ mvn.sh
@@ -1,4 +1,4 @@
 mvn clean eclipse:clean eclipse:eclipse test install -DdownloadSources=true && \
-cd blazdb-generated && \
+cd blazdb-test-generated && \
 mvn clean eclipse:clean eclipse:eclipse -DdownloadSources=true && \
-cd ../ && \
+cd ../
